f0b421a7052b9055add33a3a48cb55e7f3b78479
bmsshopt-ai <bmsshopt@gmail.com>
2025-11-28 10:33:11 +0700

    Delete utils directory

--- utils/formatter.ts
+++
@@ -1,9 +0,0 @@
-
-export const formatRupiah = (value: number, withPrefix = true): string => {
-  if (isNaN(value)) return withPrefix ? "Rp 0" : "0";
-  return `${withPrefix ? "Rp " : ""}${Math.round(value).toLocaleString("id-ID")}`;
-};
-
-export const parseRupiah = (value: string): number => {
-    return Number(value.replace(/[^0-9]/g, ''));
-};
